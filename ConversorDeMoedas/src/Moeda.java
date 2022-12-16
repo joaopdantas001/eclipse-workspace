593aff4c16563dc5b060b2b7204ba5b558776ddf
joaopdantas001 <109078725+joaopdantas001@users.noreply.github.com>
2022-12-16 14:06:34 -0300

    Add files via upload

--- ConversorDeMoedas/src/Moeda.java
+++ ConversorDeMoedas/src/Moeda.java
@@ -1,76 +1,35 @@
 
 public class Moeda {
 	
-	private String pais;
-	private String descricaomoeda;
-	private int codigosiscomex;
-	private String simbolomonetario;
-	private String simboloiso;
-	private double cambiobrl;
-	private double cambiousd;
-	private double cambioeur;
-	private double cambiogbp;
-	private double cambiojpy;
+	private String nome;
+	private String nomes;
+	private String simbolo;
+	private String iso;
 	
-	protected String getPais() {
-		return pais;
+	protected String getNome() {
+		return nome;
 	}
-	protected void setPais(String pais) {
-		this.pais = pais;
+	protected void setNome(String nome) {
+		this.nome = nome;
 	}
-	protected String getDescricaomoeda() {
-		return descricaomoeda;
+	protected String getNomes() {
+		return nomes;
 	}
-	protected void setDescricaomoeda(String descricaomoeda) {
-		this.descricaomoeda = descricaomoeda;
+	protected void setNomes(String nomes) {
+		this.nomes = nomes;
 	}
-	protected int getCodigosiscomex() {
-		return codigosiscomex;
+	protected String getSimbolo() {
+		return simbolo;
 	}
-	protected void setCodigosiscomex(int codigosiscomex) {
-		this.codigosiscomex = codigosiscomex;
+	protected void setSimbolo(String simbolo) {
+		this.simbolo = simbolo;
 	}
-	protected String getSimbolomonetario() {
-		return simbolomonetario;
+	protected String getIso() {
+		return iso;
 	}
-	protected void setSimbolomonetario(String simbolomonetario) {
-		this.simbolomonetario = simbolomonetario;
-	}
-	protected String getSimboloiso() {
-		return simboloiso;
-	}
-	protected void setSimboloiso(String simboloiso) {
-		this.simboloiso = simboloiso;
-	}
-	protected double getCambiobrl() {
-		return cambiobrl;
-	}
-	protected void setCambiobrl(double cambiobrl) {
-		this.cambiobrl = cambiobrl;
-	}
-	protected double getCambiousd() {
-		return cambiousd;
-	}
-	protected void setCambiousd(double cambiousd) {
-		this.cambiousd = cambiousd;
-	}
-	protected double getCambioeur() {
-		return cambioeur;
-	}
-	protected void setCambioeur(double cambioeur) {
-		this.cambioeur = cambioeur;
-	}
-	protected double getCambiogbp() {
-		return cambiogbp;
-	}
-	protected void setCambiogbp(double cambiogbp) {
-		this.cambiogbp = cambiogbp;
-	}
-	protected double getCambiojpy() {
-		return cambiojpy;
-	}
-	protected void setCambiojpy(double cambiojpy) {
-		this.cambiojpy = cambiojpy;
+	protected void setIso(String iso) {
+		this.iso = iso;
 	}
 	
+
 }
--- ConversorDeMoedas/src/TesteConversao.java
+++ ConversorDeMoedas/src/TesteConversao.java
@@ -1,90 +1,71 @@
-import java.text.DecimalFormat;
 
 public class TesteConversao {
 
 	public static void main(String[] args) {
 
-		TaxaDeCambio brl = new TaxaDeCambio();
+		Cambio brl = new Cambio();
 		
-		brl.setCambiobrl(1);
-		brl.setCambioeur(5.4840);
-		brl.setCambiogbp(6.3647);
-		brl.setCambiojpy(0.03821);
-		brl.setCambiousd(5.2172);
-		brl.setCodigosiscomex(790);
-		brl.setDescricaomoeda("Real");
-		brl.setPais("Brasil");
-		brl.setSimboloiso("BRL");
-		brl.setSimbolomonetario("R$");
+		brl.setIso("BRL");
+		brl.setNome("Real");
+		brl.setNomes("Reais");
+		brl.setSimbolo("R$");
+		brl.setValorcambiobrl(1);
+		brl.setValorcambioeur(0.18);
+		brl.setValorcambiojpy(25.87);
+		brl.setValorcambiousd(0.19);
 		
-		TaxaDeCambio eur = new TaxaDeCambio();
+		/*
+		 * System.out.println(brl.getIso()); System.out.println(brl.getNome());
+		 * System.out.println(brl.getSimbolo());
+		 * System.out.println(brl.getValorcambiobrl());
+		 * System.out.println(brl.getValorcambioeur());
+		 * System.out.println(brl.getValorcambiojpy());
+		 * System.out.println(brl.getValorcambiousd());
+		 * System.out.println("----------------------------");
+		 */
 		
-		eur.setCambiobrl(0.1826);
-		eur.setCambioeur(1);
-		eur.setCambiogbp(1.1612);
-		eur.setCambiojpy(0.69715);
-		eur.setCambiousd(0.9524);
-		eur.setCodigosiscomex(978);
-		eur.setDescricaomoeda("Euro");
-		eur.setPais("Uniao Europeia");
-		eur.setSimboloiso("EUR");
-		eur.setSimbolomonetario("€");
+		Cambio eur = new Cambio() ;
+			
+		eur.setIso("EUR");
+		eur.setNome("Euro");
+		eur.setNomes("Euros");
+		eur.setSimbolo("EUR$");
+		eur.setValorcambiobrl(5.60);
+		eur.setValorcambioeur(1);
+		eur.setValorcambiojpy(145.3);
+		eur.setValorcambiousd(1.06);
 		
-TaxaDeCambio gbp = new TaxaDeCambio();
+		/*
+		 * System.out.println(eur.getIso()); System.out.println(eur.getNome());
+		 * System.out.println(eur.getSimbolo());
+		 * System.out.println(eur.getValorcambiobrl());
+		 * System.out.println(eur.getValorcambioeur());
+		 * System.out.println(eur.getValorcambiojpy());
+		 * System.out.println(eur.getValorcambiousd());
+		 * System.out.println("----------------------------");
+		 */
 		
-		gbp.setCambiobrl(0.1572);
-		gbp.setCambioeur(0.8612);
-		gbp.setCambiogbp(1);
-		gbp.setCambiojpy(0.006005);
-		gbp.setCambiousd(0.9525);
-		gbp.setCodigosiscomex(540);
-		gbp.setDescricaomoeda("Libra Esterlina");
-		gbp.setPais("Gra-Bretanha");
-		gbp.setSimboloiso("GBP");
-		gbp.setSimbolomonetario("£");
+		ConversaoMoeda brlxeur = new ConversaoMoeda();
 
-TaxaDeCambio jpy = new TaxaDeCambio();
+		brlxeur.setQuantidade(50);
+		brlxeur.setValorconversao(brl.getValorcambiobrl(), brlxeur.getQuantidade(), brl.getValorcambioeur());
 		
-		jpy.setCambiobrl(26.1935);
-		jpy.setCambioeur(143.41);
-		jpy.setCambiogbp(166.54);
-		jpy.setCambiojpy(1);
-		jpy.setCambiousd(136.43);
-		jpy.setCodigosiscomex(470);
-		jpy.setDescricaomoeda("Iene Japones");
-		jpy.setPais("Japão");
-		jpy.setSimboloiso("JPY");
-		jpy.setSimbolomonetario("¥");
-		
-TaxaDeCambio usd = new TaxaDeCambio();
-		
-		usd.setCambiobrl(0.1921);
-		usd.setCambioeur(1.0517);
-		usd.setCambiogbp(1.2215);
-		usd.setCambiojpy(0.733350);
-		usd.setCambiousd(1);
-		usd.setCodigosiscomex(220);
-		usd.setDescricaomoeda("Dolar dos EUA");
-		usd.setPais("Estados Unidos");
-		usd.setSimboloiso("USD");
-		usd.setSimbolomonetario("US$");
-		
-		usd.setQuantidade(10);
-		brl.setQuantidade(20);
-		jpy.setQuantidade(30);
-		
-		System.out.println("    "                                + brl.getSimboloiso()                      + "    " + eur.getSimboloiso()                      + "    " + gbp.getSimboloiso()                      + "    " + jpy.getSimboloiso() + "   " + usd.getSimboloiso());
-		System.out.println(""     + brl.getSimboloiso() + " "    + String.format("%.2f",brl.getCambiobrl()) + "   "  + String.format("%.2f",brl.getCambioeur()) + "   "  + String.format("%.2f",brl.getCambiogbp()) + "   "  + String.format("%.2f",brl.getCambiojpy()) + "  " + String.format("%.2f",brl.getCambiousd()));
-		System.out.println(""     + eur.getSimboloiso() + " "    + String.format("%.2f",eur.getCambiobrl()) + "   "  + String.format("%.2f",eur.getCambioeur()) + "   "  + String.format("%.2f",eur.getCambiogbp()) + "   "  + String.format("%.2f",eur.getCambiojpy()) + "  " + String.format("%.2f",eur.getCambiousd()));
-		System.out.println(""     + gbp.getSimboloiso() + " "    + String.format("%.2f",gbp.getCambiobrl()) + "   "  + String.format("%.2f",gbp.getCambioeur()) + "   "  + String.format("%.2f",gbp.getCambiogbp()) + "   "  + String.format("%.2f",gbp.getCambiojpy()) + "  " + String.format("%.2f",gbp.getCambiousd()));
-		System.out.println(""     + jpy.getSimboloiso() + " "    + String.format("%.2f",jpy.getCambiobrl()) + "  "   + String.format("%.2f",jpy.getCambioeur()) + " "    + String.format("%.2f",jpy.getCambiogbp()) + " "    + String.format("%.2f",jpy.getCambiojpy()) + "  " + String.format("%.2f",jpy.getCambiousd()));
-		System.out.println(""     + usd.getSimboloiso() + " "    + String.format("%.2f",usd.getCambiobrl()) + "   "  + String.format("%.2f",usd.getCambioeur()) + "   "  + String.format("%.2f",usd.getCambiogbp()) + "   "   + String.format("%.2f",usd.getCambiojpy()) + "  " + String.format("%.2f",usd.getCambiousd()));
-		System.out.println("");
-		System.out.println("Conversao de Moedas:");
-		System.out.println(usd.getQuantidade() + " " + usd.getDescricaomoeda() + " --> " + String.format("%.2f",usd.getCambiobrl()*usd.getQuantidade()) + "    " + brl.getDescricaomoeda());
-		System.out.println(brl.getQuantidade() + " " + brl.getDescricaomoeda() + "          --> " + String.format("%.2f",brl.getCambioeur()*brl.getQuantidade()) + "  " + eur.getDescricaomoeda());
-		System.out.println(jpy.getQuantidade() + " " + jpy.getDescricaomoeda() + "  --> " + String.format("%.2f",jpy.getCambiousd()*jpy.getQuantidade()) + " " + usd.getDescricaomoeda());
 
+		System.out.println(brl.getSimbolo() + " " + brlxeur.getQuantidade() + " " +
+				brl.getNome() + " valem " + eur.getSimbolo() + " " + 
+				brlxeur.getValorconversao() + 
+				" " + eur.getNome());
+		
+		System.out.println("----------------------------");
+		
+		ConversaoMoeda eurxbrl = new ConversaoMoeda();
+
+		eurxbrl.setQuantidade(20);
+		eurxbrl.setValorconversao(eur.getValorcambioeur(), eurxbrl.getQuantidade(), eur.getValorcambiobrl());
+		
+		System.out.println(eur.getSimbolo() + " " + eurxbrl.getQuantidade() + " " + 
+				eur.getNome() + " valem " + brl.getSimbolo() + " " + 
+				eurxbrl.getValorconversao() + " " + brl.getNome());
 	}
 
 }
